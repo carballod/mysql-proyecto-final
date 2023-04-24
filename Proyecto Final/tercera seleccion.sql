f35210c859ddfc33cdb869a73e3e729ac100a9e8
Daniela <danscarballo@gmail.com>
2023-04-24 19:40:09 -0300

    Funcion cliente aleatorio

---
+++ Proyecto Final/tercera seleccion.sql
@@ -0,0 +1,16 @@
+SELECT COUNT(*) FROM clientes;
+
+SELECT * FROM clientes;
+
+SELECT * FROM clientes LIMIT 5;
+
+SELECT * FROM clientes LIMIT 5,1;
+
+SELECT * FROM clientes LIMIT 15,1;
+
+SELECT * FROM clientes LIMIT 16,1;
+
+SELECT * FROM clientes LIMIT 0,1;
+
+SELECT f_cliente_aleatorio() AS CLIENTE;
+
